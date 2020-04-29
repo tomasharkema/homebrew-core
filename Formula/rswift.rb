e6cba3151644cf35f4f49130502c373fbe8deacf
Tomas Harkema <tomas@harkema.io>
2020-04-29 22:01:28 +0200

    update rswift 5.2.2 according to new github actions build

--- Formula/rswift.rb
+++ Formula/rswift.rb
@@ -1,9 +1,8 @@
 class Rswift < Formula
   desc "Get strong typed, autocompleted resources like images, fonts and segues"
   homepage "https://github.com/mac-cain13/R.swift"
-  url "https://github.com/mac-cain13/R.swift.git",
-      :tag      => "v5.2.0",
-      :revision => "ec748b51d016e42016c93f8df8acb5e8f3635ccc"
+  url "https://github.com/mac-cain13/R.swift/releases/download/v5.2.2/rswift-v5.2.2-source.tar.gz"
+  sha256 "b27c51e9c69adcc61ed2f8a476fa5f2d97be5996d4f1e59a0e1b754a58980e26"
   head "https://github.com/mac-cain13/R.swift.git"
 
   bottle do
@@ -13,7 +12,7 @@ class Rswift < Formula
     sha256 "479d11f836c09faf090901c3326da1d5cd1d0f545be691e6c3bddb47d931eda9" => :mojave
   end
 
-  depends_on :xcode => "10.2"
+  depends_on :xcode => "11.4.1"
 
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
@@ -22,5 +21,6 @@ class Rswift < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/rswift --version")
+    assert_match "[R.swift] Failed to write out", shell_output("#{bin}/rswift generate #{testpath} 2>1&")
   end
 end
